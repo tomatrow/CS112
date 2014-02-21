b17591d247f3f0d302fad3f6675e5fab2356d4a3
AJ Caldwell <tomatrow@gmail.com>
2014-02-20 22:16:10 -0800

    Implemented calculation classes

--- Projects/week04/Chapter09Project06/src/Calculator.java
+++ Projects/week04/Chapter09Project06/src/Calculator.java
@@ -1,2 +1,55 @@
+import java.util.Arrays;
+
 public class Calculator {
+	private double _result;
+
+	Calculator() {
+		this(0.0);
+	}
+	Calculator(double result) {
+		_result = result;
+	}
+
+	public static double performOperation(Operation operation) {
+		if (operation.getOperator() == "+") {
+			return operation.getOperands()[0] + operation.getOperands()[1];
+		} else if (operation.getOperator() == "-") {
+			return operation.getOperands()[0] - operation.getOperands()[1];
+		} else if (operation.getOperator() == "*") {
+			return operation.getOperands()[0] * operation.getOperands()[1];
+		} else  { // operator == "/"
+			return operation.getOperands()[0] / operation.getOperands()[1];
+		}
+	}
+
+	// arithmetic
+	public void add(double operand) {
+		Operation op = new Operation("+",new double[]{getResult(),operand});
+		setResult(performOperation(op));
+	}
+	public void subtract(double operand) {
+		Operation op = new Operation("-",new double[]{getResult(),operand});
+		setResult(performOperation(op));	
+	}
+	public void multiply(double operand) {
+		Operation op = new Operation("*",new double[]{getResult(),operand});
+		setResult(performOperation(op));	
+	}
+	public void divide(double operand) {
+		Operation op = new Operation("/",new double[]{getResult(),operand});
+		setResult(performOperation(op));	
+	}
+	public void clear() {
+		Operation op = new Operation("*",new double[]{getResult(),0.0});
+		setResult(performOperation(op));		
+	}
+
+
+	// Boiler
+	protected void setResult(double result) {
+		_result = result;
+	}
+	public double getResult() {
+		return _result;
+	}
 }
---
+++ Projects/week04/Chapter09Project06/src/Operation.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Operation { 
+	public static final String[] VALID_OPERATORS = {"+","-","/","*"};
+	private String _operator;
+	private double[] _operands;
+
+	Operation(String operator, double[] operands) {
+		setOperator(operator);
+		setOperands(operands);
+	}
+
+	// Setters
+	private void setOperator(String operator) {
+		if (!(Arrays.asList(VALID_OPERATORS).contains(operator)))
+			throw new UnknownOperatorException(operator);
+		_operator = operator;
+	}
+	private void setOperands(double[] operands) {
+		int operandCount = operands.length;
+		boolean hasTwoElements = (operandCount == 2);
+		boolean hasNull = Arrays.asList(operands).contains(null);
+		boolean isNull = (operands == null);
+
+		if (hasTwoElements && !hasNull && !isNull)
+			_operands = operands;
+		else 
+			throw new IllegalArgumentException(Arrays.toString(operands) + " is invalid.");
+	}
+
+	// Getters
+	public String getOperator() {
+		return _operator;
+	}
+	public double[] getOperands() {
+		return  Arrays.copyOf(_operands,_operands.length);
+	}
+
+}
