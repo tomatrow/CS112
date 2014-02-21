5b02c42584083b5a5876326c0541b242bceebe6a
AJ Caldwell <tomatrow@gmail.com>
2014-02-20 18:56:24 -0800

    Make sure init doesn't overwrite existing projects

--- scripts/build.fish
+++ scripts/build.fish
@@ -12,20 +12,23 @@ set BLD_DIR $PWD/bld
 set SRC_FILES $SRC_DIR/*.java
 set MAIN_CLASS $PROJECT_NAME.java
 
-if test -e $SRC_DIR -a -e $BLD_DIR # make sure project is initialized
-	if test $argv = "build" 
-		javac -d $BLD_DIR -sourcepath $HELP_SRC_DIR $SRC_FILES
-		codemark $SRC_DIR/$MAIN_CLASS $SRC_FILES > $BLD_DIR/$PROJECT_NAME.md
-	else if test $argv = "run"
-	    java -cp $BLD_DIR:$HELP_BLD_DIR $PROJECT_NAME
-	else if test $argv = "shoot"
-		screencapture -w $BLD_DIR/$PROJECT_NAME.png 
-	end
+function masterbuild 
+	if test -e $SRC_DIR -a -e $BLD_DIR # make sure project is initialized
+		if test $argv = "build" 
+			javac -d $BLD_DIR -sourcepath $HELP_SRC_DIR $SRC_FILES
+			codemark $SRC_DIR/$MAIN_CLASS $SRC_FILES > $BLD_DIR/$PROJECT_NAME.md
+		else if test $argv = "run"
+		    java -cp $BLD_DIR:$HELP_BLD_DIR $PROJECT_NAME
+		else if test $argv = "shoot"
+			screencapture -w $BLD_DIR/$PROJECT_NAME.png 
+		end
+	else if test $argv = "init"
+		# mkdir $SRC_DIR
+		# mkdir $BLD_DIR
+		# touch README.md
+		# touch $SRC_DIR/$PROJECT_NAME.java
+	end 
+	echo $status > /dev/null # This is the only solution I found to unwanted statuses being sent to the terminal. 
 end 
 
-if test $argv = "init"
-	mkdir $SRC_DIR
-	mkdir $BLD_DIR
-	touch README.md
-	touch $SRC_DIR/$PROJECT_NAME.java
-end 
+masterbuild $argv
