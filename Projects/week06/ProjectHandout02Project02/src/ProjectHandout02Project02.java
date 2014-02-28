96c22d44fb3af4fc4afca9938686830d33a06b6d
AJ Caldwell <tomatrow@gmail.com>
2014-02-28 01:34:31 -0800

    Made my controller

--- Projects/week06/ProjectHandout02Project02/src/ProjectHandout02Project02.java
+++ Projects/week06/ProjectHandout02Project02/src/ProjectHandout02Project02.java
@@ -1,6 +1,12 @@
 /*
-# Personal Plan
 
+
+
+
+
+- - - - - - - - 
+
+# Personal Plan
 Key
 	'@' == class
 	'*' == property
@@ -12,8 +18,8 @@ Key
 
 RoadMap Start
 	Model
-	Output
 	Input
+	Output
 	Persistance 
 RoadMap End
 
@@ -37,10 +43,10 @@ ClassPlan Start
 		*petRecords #array of pets
 		-getPetRecords #array of pets
 		-setPetRecords #array of pets
-		+smallestPets #array of pets
-		+largestPets #array of pets
-		+youngestPets #array of pets
-		+oldestPets #array of pets
+		-smallestPets #array of pet
+		-largestPets #array of pets
+		-youngestPets #array of pets
+		-oldestPets #array of pets
 		-archive #serialization
 		-unarchive #deserialization
 	@PetRecordCommunicator #communicates with the user 
@@ -50,9 +56,77 @@ ClassPlan Start
 		+writeRecords
 ClassPlan End
 */
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ProjectHandout02Project02 {
+	public static final String[] VALID_CHOICES = {"y","n"};
+	public static final PetRecord[] TEST_RECORDS = {new PetRecord("Jack",10,14.3), new PetRecord("A",123456,2)};
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		PetRecordManager manager = new PetRecordManager();
+		manager.unarchive();
+
+		if (getFlowChoice("Would you like to read the data?")) {
+			String output = "All Pets:\n";
+			output = output.concat(PetRecordCommunicator.writeRecords(manager.getRecords()));
+			output = output.concat(PetRecordCommunicator.writeRecordInfo(manager.getRecords()));
+			String[] lines = output.split("\n");
+			// Make sure the output doesn't overwhelm the user 
+			for (int x = 0;x < lines.length;x++) {
+				try {
+					System.out.println(lines[x]);
+				    Thread.sleep(1000);
+				} 
+				catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+			}
+
+		} 
+		else if (getFlowChoice("Would you like to write to the data?"))  {
+			PetRecord[] inputRecords = PetRecordCommunicator.readRecords(getCount("How many records?"));
+			manager.addRecords(inputRecords);
+		}
+
+		manager.archive();
+	}
+	public static int getCount(String question) {
+		Scanner keyboard = new Scanner(System.in);
+		int choice;
+
+		System.out.println(question);
+		do { 
+			while (!keyboard.hasNextInt()) {
+				System.out.println("Enter an integer");
+				keyboard.next();
+			}
+			choice = keyboard.nextInt();
+			if (choice < 0) {
+				System.out.println("Enter a positve integer");
+			}
+		} while (choice < 0);
+		return choice;
+	}
+
+	public static boolean getFlowChoice(String question) {
+		Scanner keyboard = new Scanner(System.in);
+		String choice;
+		System.out.println(question);
+		do {
+			choice = keyboard.nextLine();
+			if (!contains(VALID_CHOICES, choice.toLowerCase())) {
+				System.out.println("You need to enter y or n");
+			}
+		} while (!contains(VALID_CHOICES, choice.toLowerCase()));
+		return (choice.toLowerCase().equals("y"))?true:false;
+	}
+	public static <T> boolean contains(T[] array, T object) {
+		boolean result = false;
+		for (T o : array) {
+			if (o.equals(object))
+				result = true;
+		}
+		return result;
 	}
 }
