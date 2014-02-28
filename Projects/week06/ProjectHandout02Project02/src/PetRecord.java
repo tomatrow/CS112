cf48e5e76b509bfec5b9a76545a115a099d46a92
AJ Caldwell <tomatrow@gmail.com>
2014-02-27 18:24:09 -0800

    Added the PetRecord model

---
+++ Projects/week06/ProjectHandout02Project02/src/PetRecord.java
@@ -0,0 +1,130 @@
+/***
+ Class for basic pet records: name, age, and weight.
+*/
+public class PetRecord
+{
+    private String name;
+    private int age;         //in years
+    private double weight;   //in pounds
+
+    public void writeOutput()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " years");
+        System.out.println("Weight: " + weight + " pounds");
+    }
+
+    public PetRecord(String initialName, int initialAge,
+                                          double initialWeight)
+    {
+        name = initialName;
+        if ((initialAge < 0) || (initialWeight < 0))
+        {
+            System.out.println("Error: Negative age or weight.");
+            System.exit(0);
+        }
+        else
+        {
+            age = initialAge;
+            weight = initialWeight;
+        }
+    }
+
+    public void set(String newName, int newAge, double newWeight)
+    {
+        name = newName;
+        if ((newAge < 0) || (newWeight < 0))
+        {
+            System.out.println("Error: Negative age or weight.");
+            System.exit(0);
+        }
+        else
+        {
+            age = newAge;
+            weight = newWeight;
+        }
+    }
+
+    public PetRecord(String initialName)
+    {
+        name = initialName;
+        age = 0;
+        weight = 0;
+    }
+
+    public void setName(String newName)
+    {
+        name = newName; 
+    }
+
+    public PetRecord(int initialAge)
+    {
+        name = "No name yet.";
+        weight = 0;
+        if (initialAge < 0)
+        {
+            System.out.println("Error: Negative age.");
+            System.exit(0);
+        }
+        else
+            age = initialAge;
+    }
+
+    public void setAge(int newAge)
+    {
+        if (newAge < 0)
+        {
+            System.out.println("Error: Negative age.");
+            System.exit(0);
+        }
+        else
+            age = newAge;        
+    }
+
+    public PetRecord(double initialWeight)
+    {
+        name = "No name yet";
+        age = 0;
+        if (initialWeight < 0)
+        {
+            System.out.println("Error: Negative weight.");
+            System.exit(0);
+        }
+        else
+            weight = initialWeight;
+    }
+
+    public void setWeight(double newWeight)
+    {
+        if (newWeight < 0)
+        {
+            System.out.println("Error: Negative weight.");
+            System.exit(0);
+        }
+        else
+            weight = newWeight; 
+    }
+
+    public PetRecord()
+    {
+        name = "No name yet.";
+        age = 0;
+        weight = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
+}
+
