71640744afdd70550c68e5125c9770a503683591
AJ Caldwell <tomatrow@gmail.com>
2014-02-27 21:50:09 -0800

    Added the getters and setters, OH YWAH

--- Projects/week06/ProjectHandout02Project02/src/PetRecordManager.java
+++ Projects/week06/ProjectHandout02Project02/src/PetRecordManager.java
@@ -1,3 +1,18 @@
+import java.util.Arrays;
+
 public class PetRecordManager {
-	
+	private PetRecord[] _records; 
+	PetRecordManager() {
+
+	}
+	// boiler
+	public getRecords() {
+		// return copies
+		PetRecord[] recordsCopy = new PetRecord[_records.length];
+		
+		for (int x = 0;x < _records.length;x++)
+			recordsCopy[x] = _records[x].copy();
+
+		return recordsCopy;
+	}
 }
