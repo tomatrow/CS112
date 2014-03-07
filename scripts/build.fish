27f51b760fef8025186c87baf22a8c8ef64f4e7f
AJ Caldwell <tomatrow@gmail.com>
2014-03-06 18:14:09 -0800

    Store sources paths in txt file and compile everything

--- scripts/build.fish
+++ scripts/build.fish
@@ -5,17 +5,26 @@ set PROJECT_NAME (basename $PWD)
 set HELPER_CLASSES_DIR /Users/ajcaldwell/Dropbox/School/14/Spring2014/CS112/Classes
 set HELP_SRC_DIR $HELPER_CLASSES_DIR/src
 set HELP_BLD_DIR $HELPER_CLASSES_DIR/bld
+set HELP_SRC_FILES $HELP_SRC_DIR/*
 
-set SRC_DIR $PWD/src
-set BLD_DIR $PWD/bld
+
+set SRC_DIR ./src
+set BLD_DIR ./bld
 
 set SRC_FILES $SRC_DIR/*.java
 set MAIN_CLASS $PROJECT_NAME.java
 
+set SOURCE_FILE_PATH ./src/source.txt
+
+
 function masterbuild 
+	touch SOURCE_FILE_PATH
+	echo $SRC_FILES > $SOURCE_FILE_PATH
+	echo $HELP_SRC_FILES >> $SOURCE_FILE_PATH
+
 	if test -e $SRC_DIR -a -e $BLD_DIR # make sure project is initialized
 		if test $argv = "build" 
-			javac -d $BLD_DIR -sourcepath $HELP_SRC_DIR $SRC_FILES
+			javac -d $BLD_DIR @$SOURCE_FILE_PATH
 			codemark $SRC_DIR/$MAIN_CLASS $SRC_FILES > $BLD_DIR/$PROJECT_NAME.md
 		else if test $argv = "run"
 		    java -cp $BLD_DIR:$HELP_BLD_DIR $PROJECT_NAME
